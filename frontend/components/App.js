48eaaab0f91ba417597e77f2b660095a3d047c6d
Mitch Hammond <mitch.b.hammond@gmail.com>
2023-12-05 11:41:26 -0700

    setting up click handlers

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,21 +3,22 @@ import Home from './Home'
 import Form from './Form'
 import{ Link, Route, Routes,} from 'react-router-dom'
 
+
+
 function App() {
   return (
     <div id="app">
       
       <nav>
         <Link to='/'>Home</Link>
-        <Link to='/order'>Order</Link>
+        <Link to='Order'>Order</Link>
       </nav>
       <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/order" element={<order />} />
-
+      <Route path="Form" element={<Form />} />
       </Routes>
       
-      {/* Route and Routes here */}
+      
       <Home />
       <Form />
     </div>
