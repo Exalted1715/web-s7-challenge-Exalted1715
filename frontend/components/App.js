17fb4235b35c49c6224f2bc921b1836e03092096
Mitch Hammond <mitch.b.hammond@gmail.com>
2023-12-05 10:47:40 -0700

    getting the routers up

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import Home from './Home'
 import Form from './Form'
+import{ Link, Route, Routes,} from 'react-router-dom'
 
 function App() {
   return (
     <div id="app">
+      
       <nav>
-        {/* NavLinks here */}
+        <Link to='/'>Home</Link>
+        <Link to='/order'>Order</Link>
       </nav>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/order" element={<order />} />
+
+      </Routes>
+      
       {/* Route and Routes here */}
       <Home />
       <Form />
     </div>
+    
   )
 }
 
--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
+import{BrowserRouter, Link, Route, Routes,} from 'react-router-dom'
 import './styles/reset.css'
 import './styles/styles.css'
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode)
 
-root.render(<App />)
+
+
+
+root.render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+    )
+
+
