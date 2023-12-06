d8b6dda3425f2d433b7d1cc942e7126d3dce1219
Mitch Hammond <mitch.b.hammond@gmail.com>
2023-12-06 12:43:45 -0700

    added trim

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -12,7 +12,7 @@ const validationErrors = {
 };
 
 const formSchema = yup.object().shape({
-  fullName: yup.string().min(3, validationErrors.fullNameTooShort).max(20, validationErrors.fullNameTooLong),
+  fullName: yup.string().trim().min(3, validationErrors.fullNameTooShort).max(20, validationErrors.fullNameTooLong).required('Full name is required'),
   size: yup.string().oneOf(['S', 'M', 'L'], validationErrors.sizeIncorrect).required('Size is required'),
 });
 
