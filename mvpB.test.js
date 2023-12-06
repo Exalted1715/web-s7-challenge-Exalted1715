057fd3aff0073f435e2c080806fe0fc49df4aa76
Mitch Hammond <mitch.b.hammond@gmail.com>
2023-12-06 12:52:43 -0700

    final changes to regular test

--- mvpB.test.js
+++ mvpB.test.js
@@ -14,7 +14,26 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [4] sum('1', 2) // returns 3
     [5] sum('10', '3') // returns 13
   */
+    
+  test('[1] sum() // throws an error "pass valid numbers"', () => {
+    expect(() => sum()).toThrow('pass valid numbers');
+  });
 
+  test('[2] sum(2, "seven") // throws an error "pass valid numbers"', () => {
+    expect(() => sum(2, 'seven')).toThrow('pass valid numbers');
+  });
+
+  test('[3] sum(1, 3) // returns 4', () => {
+    expect(sum(1, 3)).toBe(4);
+  });
+
+  test('[4] sum("1", 2) // returns 3', () => {
+    expect(sum('1', 2)).toBe(3);
+  });
+
+  test('[5] sum("10", "3") // returns 13', () => {
+    expect(sum('10', '3')).toBe(13);
+  });
   /*
   👉 TASK 2 - Integration Testing of HelloWorld component at the bottom of this module
 
@@ -29,6 +48,38 @@ describe('Sprint 7 Challenge Learner Tests', () => {
     [5] renders a text that reads "JavaScript is pretty awesome"
     [6] renders a text that includes "javaScript is pretty" (use exact = false)
   */
+
+    test('[1] renders a link that reads "Home"', () => {
+      render(<HelloWorld />);
+      expect(screen.queryByText('Home')).toBeInTheDocument();
+    });
+  
+    test('[2] renders a link that reads "About"', () => {
+      render(<HelloWorld />);
+      expect(screen.queryByText('About')).toBeInTheDocument();
+    });
+  
+    test('[3] renders a link that reads "Blog"', () => {
+      render(<HelloWorld />);
+      expect(screen.queryByText('Blog')).toBeInTheDocument();
+    });
+  
+    test('[4] renders a text that reads "The Truth"', () => {
+      render(<HelloWorld />);
+      expect(screen.queryByText('The Truth')).toBeInTheDocument();
+    });
+  
+    test('[5] renders a text that reads "JavaScript is pretty awesome"', () => {
+      render(<HelloWorld />);
+      expect(screen.queryByText('JavaScript is pretty awesome')).toBeInTheDocument();
+    });
+  
+    test('[6] renders a text that includes "javaScript is pretty" (use exact = false)', () => {
+      render(<HelloWorld />);
+      expect(screen.queryByText(/javaScript is pretty/i)).toBeInTheDocument();
+    });
+  
+
   test('you can comment out this test', () => {
     expect(true).toBe(false)
   })
